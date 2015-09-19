a753323bddaeff4932547bb81e7e97e781e72c7b
Tim Riffe <tim.riffe@gmail.com>
2015-09-20 00:49:21 +0200

    overview plot redone. Smoothing made big difference in BP line. Good thing we did it!

--- BPLE/R/lookatdata.R
+++ BPLE/R/lookatdata.R
@@ -29,11 +29,16 @@ lines(unique(LT$Year), mRLE)
 library(data.table)
 States <- data.table(States)
 Mins   <- States[,min(mxcs2),by=list(Year, Sex, Age, Cause)]
+Mins2  <- States[,min(mxc5),by=list(Year, Sex, Age, Cause)]
+
 Agg <- Mins[,sum(V1),by=list(Year, Sex, Age)]
+Agg2 <- Mins2[,sum(V1),by=list(Year, Sex, Age)]
 
 library(reshape2)
 Males <- acast(Agg[Agg$Sex == "m",], Age~Year, value.var = "V1")
 Females <- acast(Agg[Agg$Sex == "f",], Age~Year, value.var = "V1")
+Males2 <- acast(Agg2[Agg$Sex == "m",], Age~Year, value.var = "V1")
+Females2 <- acast(Agg2[Agg$Sex == "f",], Age~Year, value.var = "V1")
 
 mx2ex <- function(mx){
   lx <- c(1,exp(-cumsum(mx)),0)
@@ -42,21 +47,27 @@ mx2ex <- function(mx){
 }
 me0 <- apply(Males, 2, mx2ex)
 fe0 <- apply(Females, 2, mx2ex)
+me02 <- apply(Males2, 2, mx2ex)
+fe02 <- apply(Females2, 2, mx2ex)
 
 me0st <- acast(LT[LT$Sex == "m" & LT$Age == 0, ], Year ~ State, value.var = "ex")
 fe0st <- acast(LT[LT$Sex == "f" & LT$Age == 0, ], Year ~ State, value.var = "ex")
 
 getwd()
-png("/hdir/0/triffe/workspace/other/Decomposition/StateDivergence/Figures/ExampleBestPractices.png")
+png("Figures/ExampleBestPractices.png")
 plot(1959:2010, fRLE, type = 'l', main = "US States\nvanguard and best practices e(0)", col = "red", lty = 2, 
   ylim = c(55,90), xlab = "year", ylab = "e(0)")
 lines(1959:2010, mRLE, col = "blue", lty = 2)
 lines(1959:2004, me0,col="blue",lty=1,lwd=2)
 lines(1959:2004, fe0,col="red",lty=1,lwd=2)
+lines(1959:2004, me02,col="blue",lty=3,lwd=1)
+lines(1959:2004, fe02,col="red",lty=3,lwd=1)
+
 matplot(1959:2010, me0st, type='l',col="#0000FF20",lwd=2,lty=1,add=TRUE)
 matplot(1959:2010, fe0st, type='l',col="#FF000020",lwd=2,lty=1,add=TRUE)
-legend("bottomright", lty=c(1,1,2,2), col=c("red","blue","red","blue"),
-legend=c("Female Best Prac.","Male Best Prac.","Female vanguard","Male vanguard"))
+legend("bottomright", lty=c(1,1,3,3,2,2), col=c("red","blue","red","blue","red","blue"),
+legend=c("Female BP smoothed mxc","Male BP smoothed mxc","Female BP raw mxc5","Male BP raw mxc5",
+		"Female RLE","Male RLE"))
 dev.off()
 
 
