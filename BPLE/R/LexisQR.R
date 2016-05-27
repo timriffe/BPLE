bdcdfa0b032f7e8a171795672410af3f75d6def7
Tim Riffe <tim.riffe@gmail.com>
2016-05-27 11:32:42 +0200

    added density plot code. for both quantile regression and density code, the lowest mort often ends up with infinity e(0), which implies that there is still zero-packing going on... quantiles .99 to .1 look ok, and as expected, but .01 is junk

--- BPLE/R/LexisQR.R
+++ BPLE/R/LexisQR.R
@@ -1,10 +1,10 @@
 
 # Author: tim
 ###############################################################################
-setwd("/hdir/0/triffe/COMMONS/git/VarSC/VarSC")
-library(parallel)
-library(splines)
-library(quantreg)
+#setwd("/hdir/0/triffe/COMMONS/git/VarSC/VarSC")
+#library(parallel)
+#library(splines)
+#library(quantreg)
 # do this cause by cause
 #LTC <- local(get(load("/home/tim/Dropbox/BPLE/Data/LTC1.Rdata")))
 # LTC <- local(get(load("/hdir/0/triffe/COMMONS/Dropbox/BPLE/Data/LTC1.Rdata")))
@@ -29,8 +29,8 @@ library(quantreg)
 
 #detectCores()
 # 
-probs <- c(.99,.9,.75,.5,.25,.1,.01)
-Causes <- 1:30
+#probs <- c(.99,.9,.75,.5,.25,.1,.01)
+#Causes <- 1:30
 # The following chunk was run on Keyfitz server, using 3 cores, over the weekend
 # May 20. It may or may not finish. Went with unlogged mort and impute 0 for negatives...
 
@@ -106,11 +106,68 @@ library(LexisUtils)
 library(reshape2)
 
 # Test results when they are in, should be available in DropBox.
+Test1 <- local(get(load("/home/tim/Dropbox/BPLE/Data/QRresults.Rdata")))
+
+mat <- acast(Test1[Test1$Cause == 1 & Test1$Sex == "m", ],Age~Year,value.var = "0.99")
+mat[mat==0] <- NA
+LexisMap(mat, zlim=c(1e-6,.1))
 
 
-#mat <- acast(hm,Age~Year,value.var = "0.01")
-#mat[mat==0] <- NA
-#LexisMap(mat, zlim=c(1e-6,.1))
+source("/home/tim/git/BPLE/BPLE/R/LTuniform.R")
+
+probsc <- colnames(Test1)[-c(1:4)]
+e0QR <- expand.grid(Year = 1959:2010,Sex=c("f","m"),Quant = probs, ex=NA)
+plot(NULL, type = "n", xlim=c(1959,2010),ylim=c(50,95))
+for (pr in 1:length(probsc)){
+	Mi <- acast(Test1[Test1$Sex == "m", ],Age~Year,sum,value.var = probsc[pr])
+	Fi <- acast(Test1[Test1$Sex == "f", ],Age~Year,sum,value.var = probsc[pr])
+	
+	e0fi <- apply(Fi,2,LTuniformvecminimal,sex="f")
+	e0mi <- apply(Mi,2,LTuniformvecminimal,sex="m")
+	
+	e0QR$ex[with(e0QR,Sex == "m" & Quant == rev(probs)[pr])] <- e0mi
+	e0QR$ex[with(e0QR,Sex == "f" & Quant == rev(probs)[pr])] <- e0fi
+	
+	lines(1959:2010, e0mi, col = "blue")
+	lines(1959:2010, e0fi, col = "red")
+	
+	text(1958,e0mi["1959"],rev(probs)[pr],col="blue",xpd=TRUE)
+	text(2011,e0fi["2010"],rev(probs)[pr],col="red",xpd=TRUE)
+}
+
+
+
+Test2 <- local(get(load("/home/tim/Dropbox/BPLE/Data/CausesDensresults.Rdata")))
+
+plot(NULL, type = "n", xlim=c(1959,2010),ylim=c(50,95))
+e0Density <- expand.grid(Year = 1959:2010,Sex=c("f","m"),Quant = probs, ex=NA)
+for (pr in probs){
+	Mi <- acast(Test2[Test2$Sex == "m" & Test2$Quant == pr, ],Age~Year,sum,value.var = "Mxc")
+	Fi <- acast(Test2[Test2$Sex == "f" & Test2$Quant == pr, ],Age~Year,sum,value.var = "Mxc")
+	
+	Mi[is.infinite(Mi)] <- 0
+	Fi[is.infinite(Fi)] <- 0
+	
+	e0fi <- apply(Fi,2,LTuniformvecminimal,sex="f")
+	e0mi <- apply(Mi,2,LTuniformvecminimal,sex="m")
+    e0Density$ex[with(e0Density,Sex == "m" & Quant == pr)] <- e0mi
+	e0Density$ex[with(e0Density,Sex == "f" & Quant == pr)] <- e0fi
+	lines(1959:2010, e0mi, col = "blue")
+	lines(1959:2010, e0fi, col = "red")
+	
+	text(1958,e0mi["1959"],pr,col="blue",xpd=TRUE)
+	text(2011,e0fi["2010"],pr,col="red",xpd=TRUE)
+}
+
+
+
+
+
+
+
+
+
+
 #na0 <- function(x){
 #	x[x==0] <- NA
 #	x
